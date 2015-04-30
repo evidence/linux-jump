d9c994317cdf48ca46f952023cd9e9ad7083f481
Claudio Scordino <claudio@evidence.eu.com>
2015-04-30 16:56:57 +0200

    init.c: fix braces

--- jumpsrc/init.c
+++ jumpsrc/init.c
@@ -98,375 +98,380 @@ jiastat_t jiastat;
 
 int jump_getline(int *wordc, char wordv[Maxwords][Wordsize])
 {
-  char line[Linesize];
-  int ch;
-  int linei,wordi1,wordi2;
-  int note;
+	char line[Linesize];
+	int ch;
+	int linei,wordi1,wordi2;
+	int note;
 
-  linei=0;
-  note=0;
-  ch=getc(config);
-  if (ch=='#') note=1;
-  while ((ch != '\n') && (ch != EOF)) {
-    if ((linei < Linesize - 1) && (note == 0)) {
-      line[linei] = ch;
-      linei++;
-    }
-    ch = getc(config);
-    if (ch == '#') note = 1;
-  }
-  line[linei] = '\0';
+	linei=0;
+	note=0;
+	ch=getc(config);
+	if (ch=='#') note=1;
+	while ((ch != '\n') && (ch != EOF)) {
+		if ((linei < Linesize - 1) && (note == 0)) {
+			line[linei] = ch;
+			linei++;
+		}
+		ch = getc(config);
+		if (ch == '#') note = 1;
+	}
+	line[linei] = '\0';
 
-  for (wordi1 = 0; wordi1 < Maxwords; wordi1++)
-    wordv[wordi1][0] = '\0';
-  wordi1 = 0; linei = 0;
-  while ((line[linei] != '\0') && (wordi1 < Maxwords)) {
-    while ((line[linei] == ' ') || (line[linei] == '\t'))  
-      linei++;
-    wordi2 = 0;
-    while ((line[linei] != ' ') && (line[linei] != '\t')
-	    &&(line[linei] != '\0')) {
-      if (wordi2 < Wordsize - 1) {
-        wordv[wordi1][wordi2] = line[linei];
-        wordi2++;
-      }
-      linei++;
-    }
-    if (wordi2 > 0) { 
-      wordv[wordi1][wordi2] = '\0';  
-      wordi1++;
-    }
-  }
+	for (wordi1 = 0; wordi1 < Maxwords; wordi1++)
+		wordv[wordi1][0] = '\0';
+	wordi1 = 0; linei = 0;
+	while ((line[linei] != '\0') && (wordi1 < Maxwords)) {
+		while ((line[linei] == ' ') || (line[linei] == '\t'))  
+			linei++;
+		wordi2 = 0;
+		while ((line[linei] != ' ') && (line[linei] != '\t')
+				&&(line[linei] != '\0')) {
+			if (wordi2 < Wordsize - 1) {
+				wordv[wordi1][wordi2] = line[linei];
+				wordi2++;
+			}
+			linei++;
+		}
+		if (wordi2 > 0) { 
+			wordv[wordi1][wordi2] = '\0';  
+			wordi1++;
+		}
+	}
 
-  *wordc = wordi1;
-  return(ch == EOF);
+	*wordc = wordi1;
+	return(ch == EOF);
 }
 
 void gethosts()
 {
-  int endoffile;
-  int wordc, linec, uniquehost;
-  char wordv[Maxwords][Wordsize];
-  struct hostent *hostp;
-  int i;
+	int endoffile;
+	int wordc, linec, uniquehost;
+	char wordv[Maxwords][Wordsize];
+	struct hostent *hostp;
+	int i;
 
-  if ((config = fopen(".jiahosts", "r")) == 0) {
-    printf("Cannot open .jiahosts file\n");
-    exit(1);
-  }
+	if ((config = fopen(".jiahosts", "r")) == 0) {
+		printf("Cannot open .jiahosts file\n");
+		exit(1);
+	}
 
-  endoffile = 0;
-  hostc = 0;
-  linec = 0;
-  while (!endoffile) {
-    endoffile = jump_getline(&wordc, wordv);
-    linec++;
-    sprintf(errstr, "Line %4d: incorrect host specification!", linec);
-    assert0(((wordc == Wordnum) || (wordc == 0)), errstr);
-    if (wordc != 0) {
-      hostp=gethostbyname(wordv[0]);
-      sprintf(errstr, "Line %4d: incorrect host %s!", linec, wordv[0]);
-      assert0((hostp != NULL), errstr);
-      strcpy(hosts[hostc].name, hostp->h_name); 
-      memcpy(hosts[hostc].addr, hostp->h_addr, hostp->h_length);
-      hosts[hostc].addrlen = hostp->h_length;
-      strcpy(hosts[hostc].user, wordv[1]); 
-      strcpy(hosts[hostc].passwd, wordv[2]);
+	endoffile = 0;
+	hostc = 0;
+	linec = 0;
+	while (!endoffile) {
+		endoffile = jump_getline(&wordc, wordv);
+		linec++;
+		sprintf(errstr, "Line %4d: incorrect host specification!", linec);
+		assert0(((wordc == Wordnum) || (wordc == 0)), errstr);
+		if (wordc != 0) {
+			hostp=gethostbyname(wordv[0]);
+			sprintf(errstr, "Line %4d: incorrect host %s!", linec, wordv[0]);
+			assert0((hostp != NULL), errstr);
+			strcpy(hosts[hostc].name, hostp->h_name); 
+			memcpy(hosts[hostc].addr, hostp->h_addr, hostp->h_length);
+			hosts[hostc].addrlen = hostp->h_length;
+			strcpy(hosts[hostc].user, wordv[1]); 
+			strcpy(hosts[hostc].passwd, wordv[2]);
 
-      for (i = 0; i < hostc; i++) {
+			for (i = 0; i < hostc; i++) {
 #ifdef NFS
-        uniquehost = (strcmp(hosts[hostc].name, hosts[i].name) != 0);
+				uniquehost = (strcmp(hosts[hostc].name, hosts[i].name) != 0);
 #else  /* NFS */
-        uniquehost = ((strcmp(hosts[hostc].addr, hosts[i].addr) != 0) ||
-                     (strcmp(hosts[hostc].user, hosts[i].user) != 0));
+				uniquehost = ((strcmp(hosts[hostc].addr, hosts[i].addr) != 0) ||
+						(strcmp(hosts[hostc].user, hosts[i].user) != 0));
 #endif /*NFS */
-        sprintf(errstr, "Line %4d: repeated spec of the same host!",
-		linec);
-        assert0(uniquehost, errstr);
-      }
-      hostc++; 
-    } 
-  }
+				sprintf(errstr, "Line %4d: repeated spec of the same host!",
+						linec);
+				assert0(uniquehost, errstr);
+			}
+			hostc++; 
+		} 
+	}
 
-  assert0((hostc <= Maxhosts), "Too many hosts!");
-  fclose(config);
+	assert0((hostc <= Maxhosts), "Too many hosts!");
+	fclose(config);
 }
 
 
 void copyfiles(char *argv)
 {
-  int hosti, rcpyes;
-  char cmd[Linesize];
+	int hosti, rcpyes;
+	char cmd[Linesize];
 
-  printf("******Start to copy system files to slaves!******\n");
+	printf("******Start to copy system files to slaves!******\n");
 
-  for (hosti = 1; hosti < hostc; hosti++) {
-    printf("Copy files to %s@%s.\n", hosts[hosti].user, hosts[hosti].name);
+	for (hosti = 1; hosti < hostc; hosti++) {
+		printf("Copy files to %s@%s.\n", hosts[hosti].user, hosts[hosti].name);
 
-    cmd[0] = '\0';
-    strcat(cmd, "rcp .jiahosts ");
-    strcat(cmd, hosts[hosti].user);
-    strcat(cmd, "@");
-    strcat(cmd, hosts[hosti].name);
-    strcat(cmd, ":");
-    rcpyes = system(cmd);
-    sprintf(errstr, "Cannot rcp .jiahosts to %s!\n", hosts[hosti].name);
-    assert0((rcpyes == 0), errstr);
+		cmd[0] = '\0';
+		strcat(cmd, "rcp .jiahosts ");
+		strcat(cmd, hosts[hosti].user);
+		strcat(cmd, "@");
+		strcat(cmd, hosts[hosti].name);
+		strcat(cmd, ":");
+		rcpyes = system(cmd);
+		sprintf(errstr, "Cannot rcp .jiahosts to %s!\n", hosts[hosti].name);
+		assert0((rcpyes == 0), errstr);
 
-    cmd[0] = '\0';
-    strcat(cmd, "rcp ");
-    strcat(cmd, argv);
-    strcat(cmd, " ");
-    strcat(cmd, hosts[hosti].user);
-    strcat(cmd, "@");
-    strcat(cmd, hosts[hosti].name);
-    strcat(cmd, ":");
-    rcpyes = system(cmd);
-    sprintf(errstr, "Cannot rcp %s to %s!\n", argv, hosts[hosti].name);
-    assert0((rcpyes == 0),errstr);
-  } 
-  printf("Remote copy succeed!\n\n");
+		cmd[0] = '\0';
+		strcat(cmd, "rcp ");
+		strcat(cmd, argv);
+		strcat(cmd, " ");
+		strcat(cmd, hosts[hosti].user);
+		strcat(cmd, "@");
+		strcat(cmd, hosts[hosti].name);
+		strcat(cmd, ":");
+		rcpyes = system(cmd);
+		sprintf(errstr, "Cannot rcp %s to %s!\n", argv, hosts[hosti].name);
+		assert0((rcpyes == 0),errstr);
+	} 
+	printf("Remote copy succeed!\n\n");
 }
 
 void startprocs(int argc, char **argv)
 {
-  
-#ifdef NFS
-  char *pwd;
-#endif /* NFS*/
-  int hosti;
-  char cmd[Linesize], *hostname;
-  int i;
- 
-  printf("******Start to create processes on slaves!******\n\n");
 
 #ifdef NFS
-  sprintf(errstr, "Failed to get current working directory");
-  pwd = getenv("PWD"); 
-  assert0((pwd != NULL), errstr);
+	char *pwd;
+#endif /* NFS*/
+	int hosti;
+	char cmd[Linesize], *hostname;
+	int i;
+
+	printf("******Start to create processes on slaves!******\n\n");
+
+#ifdef NFS
+	sprintf(errstr, "Failed to get current working directory");
+	pwd = getenv("PWD"); 
+	assert0((pwd != NULL), errstr);
 #endif /* NFS */
 
-  Startport = getpid();
-  assert0( (Startport != -1), "getpid() error");
-  Startport = 10000 + (Startport * Maxhosts * Maxhosts * 4) % 10000;
+	Startport = getpid();
+	assert0( (Startport != -1), "getpid() error");
+	Startport = 10000 + (Startport * Maxhosts * Maxhosts * 4) % 10000;
 
 #ifdef LINUX 
-  for (hosti = 1; hosti < hostc; hosti++) {
+	for (hosti = 1; hosti < hostc; hosti++) {
 #ifdef NFS
-    sprintf(cmd, "cd %s; %s", pwd, pwd);
+		sprintf(cmd, "cd %s; %s", pwd, pwd);
 #else  
-    cmd[0]='\0';
-    strcat(cmd, "rsh -l ");
-    strcat(cmd, hosts[hosti].user);
+		cmd[0]='\0';
+		strcat(cmd, "rsh -l ");
+		strcat(cmd, hosts[hosti].user);
 #endif 
-    hostname = hosts[hosti].name;
-    strcat(cmd, " ");
-    strcat(cmd, hostname); 
-    strcat(cmd, " ");
-    for (i = 0;i < argc; i++) {
-      strcat(cmd, argv[i]);
-      strcat(cmd, " ");
-    }
+		hostname = hosts[hosti].name;
+		strcat(cmd, " ");
+		strcat(cmd, hostname); 
+		strcat(cmd, " ");
+		for (i = 0;i < argc; i++) {
+			strcat(cmd, argv[i]);
+			strcat(cmd, " ");
+		}
 
-    strcat(cmd, "-P");
-    sprintf(cmd, "%s%ld ", cmd, Startport);
-    strcat(cmd," &");
-    printf("Starting CMD %s on host %s\n", cmd, hosts[hosti].name);
+		strcat(cmd, "-P");
+		sprintf(cmd, "%s%ld ", cmd, Startport);
+		strcat(cmd," &");
+		printf("Starting CMD %s on host %s\n", cmd, hosts[hosti].name);
 #ifdef NFS
-    system(cmd);   
+		system(cmd);   
 #else  
-    system(cmd);
+		system(cmd);
 #endif
-#else 
-  for (hosti = 1; hosti < hostc; hosti++) {
+		sprintf(errstr, "Fail to start process on %s!", hosts[hosti].name);
+		assert0((hosts[hosti].riofd != -1), errstr);
+	}
+#else /* LINUX */
+	for (hosti = 1; hosti < hostc; hosti++) {
 #ifdef NFS
-    sprintf(cmd, "cd %s; %s", pwd, pwd);
+		sprintf(cmd, "cd %s; %s", pwd, pwd);
 #else  /* NFS */
-    cmd[0] = '\0';
-    strcat(cmd, "~");
-    strcat(cmd, hosts[hosti].user);
+		cmd[0] = '\0';
+		strcat(cmd, "~");
+		strcat(cmd, hosts[hosti].user);
 #endif /* NFS */
-    strcat(cmd, "/");
-    for (i = 0; i < argc; i++){
-      strcat(cmd, argv[i]);
-      strcat(cmd, " ");
-    }
-    strcat(cmd, "-P");
-    sprintf(cmd,"%s%d ", cmd, Startport);
+		strcat(cmd, "/");
+		for (i = 0; i < argc; i++){
+			strcat(cmd, argv[i]);
+			strcat(cmd, " ");
+		}
+		strcat(cmd, "-P");
+		sprintf(cmd,"%s%d ", cmd, Startport);
 
-    printf("Starting CMD %s on host %s\n", cmd, hosts[hosti].name);
-    sp = getservbyname("exec", "tcp");
-    assert0((sp != NULL), "exec/tcp: unknown service!");
-    hostname = hosts[hosti].name;
+		printf("Starting CMD %s on host %s\n", cmd, hosts[hosti].name);
+		sp = getservbyname("exec", "tcp");
+		assert0((sp != NULL), "exec/tcp: unknown service!");
+		hostname = hosts[hosti].name;
 
 #ifdef NFS
-    hosts[hosti].riofd = rexec(&hostname, sp->s_port, NULL, NULL,
-                               cmd, &(hosts[hosti].rerrfd));
+		hosts[hosti].riofd = rexec(&hostname, sp->s_port, NULL, NULL,
+				cmd, &(hosts[hosti].rerrfd));
 #else  /* NFS */
-    hosts[hosti].riofd = rexec(&hostname, sp->s_port, hosts[hosti].user,
-                               hosts[hosti].passwd, cmd,
-			       &(hosts[hosti].rerrfd));
+		hosts[hosti].riofd = rexec(&hostname, sp->s_port, hosts[hosti].user,
+				hosts[hosti].passwd, cmd,
+				&(hosts[hosti].rerrfd));
 #endif /* NFS */
-#endif
-    sprintf(errstr, "Fail to start process on %s!", hosts[hosti].name);
-    assert0((hosts[hosti].riofd != -1), errstr);
-  }
+		sprintf(errstr, "Fail to start process on %s!", hosts[hosti].name);
+		assert0((hosts[hosti].riofd != -1), errstr);
+	}
+#endif /* LINUX */
 }
 
 int mypid()
 {
-  char hostname[Wordsize];
-  uid_t uid;
-  struct passwd *userp;
-  struct hostent *hostp;
-  int i;
-  
-  assert0((gethostname(hostname, Wordsize) == 0), "Cannot get hostname!");
-  hostp = gethostbyname(hostname);
-  assert0((hostp != NULL), "Cannot get host address!");
+	char hostname[Wordsize];
+	uid_t uid;
+	struct passwd *userp;
+	struct hostent *hostp;
+	int i;
 
-  uid = getuid();
-  userp = getpwuid(uid);
-  assert0((userp != NULL), "Cannot get user name!");
+	assert0((gethostname(hostname, Wordsize) == 0), "Cannot get hostname!");
+	hostp = gethostbyname(hostname);
+	assert0((hostp != NULL), "Cannot get host address!");
+	printf("hostname = %s\n", hostname);
+	printf("hostp = %s\n", hostp->h_name);
 
-  i = 0;
-  strtok(hostname, ".");
-  while ((i<hostc) &&
+	uid = getuid();
+	userp = getpwuid(uid);
+	assert0((userp != NULL), "Cannot get user name!");
+
+	i = 0;
+	strtok(hostname, ".");
+	while ((i<hostc) &&
 #ifdef NFS
-         (!(strncmp(hosts[i].name, hostname, strlen(hostname)) == 0)))
+			(!(strncmp(hosts[i].name, hostname, strlen(hostname)) == 0)))
 #else  /* NFS */
-         (!((strncmp(hosts[i].name, hostname, strlen(hostname)) == 0) &&
-            (strcmp(hosts[i].user, userp->pw_name) == 0)))) 
+		(!((strncmp(hosts[i].name, hostname, strlen(hostname)) == 0) &&
+		   (strcmp(hosts[i].user, userp->pw_name) == 0)))) 
 #endif /* NFS */
-    i++;
- 
-  assert0((i < hostc), "Get Process id incorrect");
-  return(i);
+		   i++;
+
+	assert0((i < hostc), "Get Process id incorrect");
+	return(i);
 }
 
 void jiacreat(int argc, char **argv)
 {
-  gethosts();
-  if (hostc == 0) {
-    printf("  No hosts specified!\n");
-    exit(0);
-  }
-  jia_pid = mypid();
+	gethosts();
+	if (hostc == 0) {
+		printf("  No hosts specified!\n");
+		exit(0);
+	}
+	jia_pid = mypid();
 
-  if (jia_pid == 0) { 
-/*
-    printf("*********Total of %d hosts found!**********\n\n", hostc);
+	if (jia_pid == 0) { 
+		/*
+		   printf("*********Total of %d hosts found!**********\n\n", hostc);
 #ifndef NFS
-    copyfiles(argv[0]); 
+copyfiles(argv[0]); 
 #endif
 */
-    sleep(1);
-    startprocs(argc, argv);
-  } else {
-    int c;
-    optind = 1;
-    while ((c = getopt(argc, argv, "P:")) != -1) {
-      switch (c) {
-        case 'P': {
-          Startport = atol(optarg);
-          break;
-        }
-      }
-    }
-    optind = 1;
-  }
+		sleep(1);
+		startprocs(argc, argv);
+	} else {
+		int c;
+		optind = 1;
+		while ((c = getopt(argc, argv, "P:")) != -1) {
+			switch (c) {
+				case 'P': {
+						  Startport = atol(optarg);
+						  break;
+					  }
+			}
+		}
+		optind = 1;
+	}
 }
 
 void barrier0()
 {
-  int hosti;
-  char buf[4];
+	int hosti;
+	char buf[4];
 
-  if (jia_pid == 0) {
-    for (hosti = 1; hosti < hostc; hosti++) {
-      printf("Poll host %d: stream %4d----", hosti, hosts[hosti].riofd);
-      read(hosts[hosti].riofd, buf, 3);
-      buf[3] = '\0';
-      printf("%s Host %4d arrives!\n", buf, hosti);
+	if (jia_pid == 0) {
+		for (hosti = 1; hosti < hostc; hosti++) {
+			printf("Poll host %d: stream %4d----", hosti, hosts[hosti].riofd);
+			read(hosts[hosti].riofd, buf, 3);
+			buf[3] = '\0';
+			printf("%s Host %4d arrives!\n", buf, hosti);
 #ifdef NFS
-      write(hosts[hosti].riofd, "ok!", 3);
+			write(hosts[hosti].riofd, "ok!", 3);
 #endif
-    }
+		}
 #ifndef NFS
-    for (hosti = 1; hosti < hostc; hosti++)
-      write(hosts[hosti].riofd, "ok!", 3);
+		for (hosti = 1; hosti < hostc; hosti++)
+			write(hosts[hosti].riofd, "ok!", 3);
 #endif
-  } 
-  else {
-    write(1, "ok?", 3);
-    read(0, buf, 3);
-  }
+	} 
+	else {
+		write(1, "ok?", 3);
+		read(0, buf, 3);
+	}
 }
 
 void redirstdio()
 {
-  char outfile[Wordsize];
+	char outfile[Wordsize];
 
-  if (jia_pid != 0) {
+	if (jia_pid != 0) {
 #ifdef NFS
-    sprintf(outfile,"debug-%d.log", jia_pid);
+		sprintf(outfile,"debug-%d.log", jia_pid);
 #else
-    sprintf(outfile,"debug-%d.log", jia_pid);
+		sprintf(outfile,"debug-%d.log", jia_pid);
 #endif
-    freopen(outfile, "w", stdout);
-    setbuf(stdout, NULL);
+		freopen(outfile, "w", stdout);
+		setbuf(stdout, NULL);
 #ifdef NFS
-    sprintf(outfile, "debug-%d.err", jia_pid);
+		sprintf(outfile, "debug-%d.err", jia_pid);
 #else
-    sprintf(outfile, "debug-%d.err", jia_pid);
+		sprintf(outfile, "debug-%d.err", jia_pid);
 #endif
-    freopen(outfile, "w", stderr);
-    setbuf(stderr, NULL);
-  }
+		freopen(outfile, "w", stderr);
+		setbuf(stderr, NULL);
+	}
 }
 
 void jia_init(int argc, char **argv)
 {
-  struct rlimit rl;
+	struct rlimit rl;
 
-  strcpy(argv0, argv[0]);
-  disable_sigio();
-  jia_lock_index = 0;  
-  jiacreat(argc, argv);
+	strcpy(argv0, argv[0]);
+	disable_sigio();
+	jia_lock_index = 0;  
+	jiacreat(argc, argv);
 #if defined SOLARIS || defined LINUX
-  sleep(2);
-  rl.rlim_cur = Maxfileno;
-  rl.rlim_max = Maxfileno;
-  setrlimit(RLIMIT_NOFILE, &rl);
+	sleep(2);
+	rl.rlim_cur = Maxfileno;
+	rl.rlim_max = Maxfileno;
+	setrlimit(RLIMIT_NOFILE, &rl);
 #endif /* SOLARIS */
 
-  rl.rlim_cur = Maxmemsize;
-  rl.rlim_max = Maxmemsize;
-  setrlimit(RLIMIT_DATA, &rl);
+	rl.rlim_cur = Maxmemsize;
+	rl.rlim_max = Maxmemsize;
+	setrlimit(RLIMIT_DATA, &rl);
 
-  initmem();
-  initsyn();
-  initcomm();
+	initmem();
+	initsyn();
+	initcomm();
 #ifdef DOSTAT
-  clearstat();
+	clearstat();
 #endif
 #ifndef LINUX
-  barrier0();
+	barrier0();
 #else
-  sleep(2);
+	sleep(2);
 #endif
-  redirstdio();
-  enable_sigio();
+	redirstdio();
+	enable_sigio();
 
-  jia_current_time();
-  jia_clock();
-  if (jia_pid != 0) sleep(1);
+	jia_current_time();
+	jia_clock();
+	if (jia_pid != 0) sleep(1);
 }
 
 #ifdef DOSTAT
 void clearstat()
 {
-  memset((char*)&jiastat, 0, sizeof(jiastat));
+	memset((char*)&jiastat, 0, sizeof(jiastat));
 }
 #endif
 
@@ -479,7 +484,7 @@ int hostc = 1;
 
 void jia_init(int argc, char **argv)
 {
-  printf("This is JUMP-NULL\n");
+	printf("This is JUMP-NULL\n");
 }
 #endif /* NULL_LIB */
 
@@ -488,16 +493,16 @@ unsigned int t_start, t_stop = 0;
 unsigned int jia_startstat()
 {
 #ifdef DOSTAT
-   clearstat();
+	clearstat();
 #endif
-   t_start = get_usecs();
-   return t_start;
+	t_start = get_usecs();
+	return t_start;
 }
 
 unsigned int jia_stopstat()
 {
 #ifdef DOSTAT
 #endif
-   t_stop = get_usecs();
-   return t_stop;
+	t_stop = get_usecs();
+	return t_stop;
 }
